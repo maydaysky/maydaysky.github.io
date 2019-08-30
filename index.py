cc57bd889d2a2e1a91c5ae6e221034ec09e0e90f
maydaysky <54716796+maydaysky@users.noreply.github.com>
2019-08-30 15:59:30 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-print "Content-type:text/html"
-print                               # 空行，告诉服务器结束头部
-print
+print( "Content-type:text/html")
+print()                               # 空行，告诉服务器结束头部
+print('''<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>hello,world</title>
+</head>
+<body>
+<h1>Hello world</h1>
+<p>hello world</p>
+<img src="http://www.runoob.com/images/logo.png/" width="258" height="39" />
+</body>
+</html>
+''')
